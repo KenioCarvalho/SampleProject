d5d2f8dd5429605a378f61296587b94808429d6a
KenioCarvalho <kenio.carvalho@plansis.com.br>
2017-08-14 10:09:58 -0300

    initial commit for Sample Project

---
+++ SampleProject/src/br/com/mysphere/WikiArticle.java
@@ -0,0 +1,40 @@
+package br.com.mysphere;
+
+public class WikiArticle {
+
+	String title;
+	String author;
+	String content;
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+
+	}
+
+}
